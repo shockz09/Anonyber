71d1156b7ca484af0b557979ebbfa28deea931d9
Rohit <rohit200827@gmail.com>
2022-03-28 17:57:57 +0530

    Revert "Fix: Add Navbar And Footer To Quiz Grid"
    This reverts commit fbf267231447af0d72e1bc1f6961946afd998e38.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <hr className="hr" />
-      <p className="text-hecker text-center text-2xl">&copy; Anonyber 2022 </p>
-    </footer>
+<div>
+    <hr className="hr"/> 
+    <p className="text-hecker text-center text-2xl">&copy; Anonyber  2022 </p>
+    </div>
   );
 }
 
