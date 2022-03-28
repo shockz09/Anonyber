efb2c62f0936ca4ad89960a887cc2c9859e3fff2
Dev2212 <developervaarun@gmail.com>
2022-03-28 17:43:13 +0530

    Fix: Add Navbar And Footer To Quiz Grid

--- src/pages/QuizGrid.jsx
+++ src/pages/QuizGrid.jsx
@@ -1,50 +1,57 @@
 import QuizCard from "../components/QuizCard";
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import React from "react";
 
 function QuizGrid() {
   return (
-    <main className="text-center text-white">
-      <h1 className="underline decoration-hecker text-5xl m-24">Quizzes</h1>
-      <div className="flex gap-10 items-center justify-center flex-wrap ml-16 mr-16">
-        <QuizCard
-          img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
-          quiz_title="Quiz"
-          level="Beginner"
-          quiz_focus="Basic Level CSS"
-        />
-        <QuizCard
-          img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
-          quiz_title="Quiz"
-          level="Beginner"
-          quiz_focus="Basic Level CSS"
-        />
-        <QuizCard
-          img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
-          quiz_title="Quiz"
-          level="Beginner"
-          quiz_focus="Basic Level CSS"
-        />
-        <QuizCard
-          img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
-          quiz_title="Quiz"
-          level="Beginner"
-          quiz_focus="Basic Level CSS"
-        />
-        <QuizCard
-          img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
-          quiz_title="Quiz"
-          level="Beginner"
-          quiz_focus="Basic Level CSS"
-        />
-        <QuizCard
-          img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
-          quiz_title="Quiz"
-          level="Beginner"
-          quiz_focus="Basic Level CSS"
-        />
-      </div>
-    </main>
+    <body>
+      <header>
+        <Navbar />
+      </header>
+      <main className="text-center text-white">
+        <h1 className="underline decoration-hecker text-5xl m-24">Quizzes</h1>
+        <div className="flex gap-10 items-center justify-center flex-wrap ml-16 mr-16">
+          <QuizCard
+            img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
+            quiz_title="Quiz"
+            level="Beginner"
+            quiz_focus="Basic Level CSS"
+          />
+          <QuizCard
+            img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
+            quiz_title="Quiz"
+            level="Beginner"
+            quiz_focus="Basic Level CSS"
+          />
+          <QuizCard
+            img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
+            quiz_title="Quiz"
+            level="Beginner"
+            quiz_focus="Basic Level CSS"
+          />
+          <QuizCard
+            img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
+            quiz_title="Quiz"
+            level="Beginner"
+            quiz_focus="Basic Level CSS"
+          />
+          <QuizCard
+            img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
+            quiz_title="Quiz"
+            level="Beginner"
+            quiz_focus="Basic Level CSS"
+          />
+          <QuizCard
+            img_src="https://codingal.s3.amazonaws.com/media/quizcontest/thumbnail/2021/12/24/27.png"
+            quiz_title="Quiz"
+            level="Beginner"
+            quiz_focus="Basic Level CSS"
+          />
+        </div>
+      </main>
+      <Footer />
+    </body>
   );
 }
 
