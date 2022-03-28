ece52bc05d1421a593403c1aa28e65c9214f8aba
Rohit <rohit200827@gmail.com>
2022-03-28 16:00:36 +0530

    update

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -189,7 +189,7 @@ function Quiz() {
 
   const buttonclicknext = () => {
     if (
-      (currentindex < quizData.length && checkClickA) || checkClickB || checkClickC || checkClickD
+      currentindex < quizData.length && (checkClickA || checkClickB || checkClickC || checkClickD)
     ) {
       setCurrentIndex(currentindex + 1);
       console.log("done log");
@@ -200,12 +200,12 @@ function Quiz() {
       console.log(quizData.length);
       setCheckClickC(false);
       setCheckClickD(false);
-    } else if (currentindex > quizData.length) {
+    } else if (currentindex > quizData.length && (checkClickA || checkClickB || checkClickC || checkClickD)) {
       let fulldiv = document.querySelector(".main-div-quiz");
       fulldiv.innerHTML = `<p className="text-score">Your Score is ${score} out of ${quizData.length}</p>`;
       console.log("bruh kek");
       fulldiv.classList.add("text-score");
-    } else if (currentindex === quizData.length) {
+    } else if (currentindex === quizData.length && (checkClickA || checkClickB || checkClickC || checkClickD)) {
       console.log("index equal");
     } 
   };
